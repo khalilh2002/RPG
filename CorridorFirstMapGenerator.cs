55298c94acf2376dc9ddb4f5e45593df95d4ed9a
khalilh2002 <khalil.houssein2002@gmail.com>
2024-02-25 19:59:34 +0100

    triple

--- CorridorFirstMapGenerator.cs
+++ CorridorFirstMapGenerator.cs
@@ -21,7 +21,9 @@ public class CorridorFirstMapGenerator : simpleWalkMapGenerator
         HashSet<Vector2Int> floorPosition = new HashSet<Vector2Int>();
         HashSet<Vector2Int> potentialRoomPosition = new HashSet<Vector2Int>();
 
-        createCorridor(floorPosition , potentialRoomPosition);
+        List<List<Vector2Int>> corridors =  createCorridor(floorPosition , potentialRoomPosition);
+        
+
         HashSet<Vector2Int> roomPositions = createRoom(potentialRoomPosition);
 
         List<Vector2Int> deadEnds = FindDeadEnd(floorPosition);
@@ -29,8 +31,31 @@ public class CorridorFirstMapGenerator : simpleWalkMapGenerator
 
         floorPosition.UnionWith(roomPositions);
 
+        //triple the size of the corridors 
+        for (int i = 0; i < corridors.Count; i++)
+        {
+            corridors[i] = insert3x3(corridors[i]).ToList<Vector2Int>();
+            floorPosition.UnionWith(corridors[i]);
+        }
+
         tilmapVisulaizer.paintFloorTiles(floorPosition);
         WallGenerator.createWalls(floorPosition,tilmapVisulaizer);
+    }
+
+    private HashSet<Vector2Int> insert3x3(List<Vector2Int> corrider)
+    {
+        HashSet<Vector2Int> corriderFix = new HashSet<Vector2Int>();
+        for (int i = 1; i < corrider.Count; i++)
+        {
+            for (int x = 1; x < 2; x++)
+            {
+                for (int y = 0; y < 2; y++)
+                {
+                    corriderFix.Add(corrider[i-1]+new Vector2Int(x,y));
+                }
+            }
+        }
+        return corriderFix;
     }
 
     private void createDeadEndRooms(List<Vector2Int> deadEnds, HashSet<Vector2Int> roomPositions)
@@ -88,17 +113,25 @@ public class CorridorFirstMapGenerator : simpleWalkMapGenerator
         return roomPositions;
     }
 
-    private void createCorridor(HashSet<Vector2Int> floorPosition , HashSet<Vector2Int> potentialRoomPosition )
+    private List<List<Vector2Int>> createCorridor(HashSet<Vector2Int> floorPosition , HashSet<Vector2Int> potentialRoomPosition )
     {
         var currentPosition = startPosition;
         potentialRoomPosition.Add(currentPosition);
+
+        List<List<Vector2Int>> corridors_all = new List<List<Vector2Int>>();
+
         for (int i = 0; i < corridorCount; i++)
         {
             var corrodor = GenerateMapAlgorithm.RandomWalkCorridor(currentPosition,corridorLenght);
             //Stack (Pile) -> pop(depile)
+            
+            corridors_all.Add(corrodor);
+
             currentPosition = corrodor [ corrodor.Count - 1 ] ; 
             potentialRoomPosition.Add(currentPosition);
             floorPosition.UnionWith(corrodor);
         }
+
+        return corridors_all;
     }
 }
